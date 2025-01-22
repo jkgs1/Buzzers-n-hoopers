03d96fa31ba1e6524116b8da306eee284254c643
filras <filip45.rask@gmail.com>
2025-01-22 15:53:04 +0100

    Fixed issues with matchup models

--- matchup/models.py
+++ matchup/models.py
@@ -3,18 +3,17 @@ from clubber.models import Team
 from clubber.models import Player 
 from common.models import User
 # Create your models here.
-
 class Match(models.Model):
     # Support for series yet to be implemented 
     #  seriesId = models.ForeignKey(Series, null=True, blank=True)
-    homeTeamId = models.ForeignKey(Team, null=False)
-    awayTeamId = models.ForeignKey(Team, null=False)
-    events = models.ManyToManyField(Event)
+    homeTeamId = models.ForeignKey(Team, related_name='homeId', null=False, on_delete=models.CASCADE)
+    awayTeamId = models.ForeignKey(Team, related_name='awayId', null=False, on_delete=models.CASCADE)
+     
 
 class Event(models.Model):
-    match = models.ForeignKey(Match, on_delete=models.SET_NULL, null=False)
-    player1 = models.ForeignKey(MatchPlayer)
-    player2 = models.ForeignKey(MatchPlayer)
+    match = models.ForeignKey(Match, null=False, blank=False, on_delete=models.CASCADE)
+    player1 = models.ForeignKey("MatchPlayer", related_name='p1', on_delete=models.CASCADE)
+    player2 = models.ForeignKey("MatchPlayer", related_name='p2', on_delete=models.CASCADE)
     EVENT_TYPE = [
         ("1P", "1 Point"),
         ("2P", "2 Points"),
@@ -23,16 +22,17 @@ class Event(models.Model):
         ("FO", "Other foul"),
         ("EX", "Exchange"),
         ("TO", "Timeout")
-        ]
+ 
+  ]
 
 class MatchPlayer(models.Model):
-    match = models.ForeignKey(Match)
-    player = models.ForeignKey(Player, null=True, blank=True)
-    team = models.ForeignKey(Team, null=True, blank=True)
+    match = models.ForeignKey(Match, on_delete=models.CASCADE)
+    player = models.ForeignKey(Player, null=True, blank=True, on_delete=models.CASCADE)
+    team = models.ForeignKey(Team, null=True, blank=True, on_delete=models.CASCADE)
 
 class MatchAdmin(models.Model):
-    user = models.ForeignKey(User)
-    match = models.OneToOneField(Match)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    match = models.OneToOneField(Match, on_delete=models.CASCADE)
     PERMISSIONS = [
     
             ]
