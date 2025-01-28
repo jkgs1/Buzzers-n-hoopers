a6c3cb6e453c5975dbd449984b334323ccd097fe
filras <filip45.rask@gmail.com>
2025-01-28 16:33:36 +0100

    matchup api

---
+++ matchup/serializers.py
@@ -0,0 +1,22 @@
+from matchup.models import *
+from rest_framework import serializers
+
+class MatchSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Match
+        fields = '__all__'
+
+class EventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Event
+        fields = '__all__'
+
+class MatchPlayerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MatchPlayer
+        fields = '__all__'
+
+class MatchAdminSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MatchAdmin
+        fields = '__all__'
